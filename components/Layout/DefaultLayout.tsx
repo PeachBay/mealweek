edb1ae7e0b427a6f85c1824de3c08ac1f6a717f2
Tiffeny <6912905+PeachBay@users.noreply.github.com>
2022-12-18 20:31:19 -0500

    fix default layout

--- components/Layout/DefaultLayout.tsx
+++ components/Layout/DefaultLayout.tsx
@@ -1,80 +1,10 @@
 import { Container } from '@mantine/core';
-import { DefaultNavbar } from '../Navbar/DefaultNavbar';
-import { Footer } from '../Footer/Footer';
 
-type DefaultLayoutProps = {
+interface DefaultLayoutProps {
   children: React.ReactNode;
-};
+}
 
 export default function DefaultLayout({ children }: DefaultLayoutProps) {
-  const data = {
-    data: [
-      {
-        title: 'About',
-        links: [
-          {
-            label: 'Features',
-            link: '#',
-          },
-          {
-            label: 'Pricing',
-            link: '#',
-          },
-          {
-            label: 'Support',
-            link: '#',
-          },
-          {
-            label: 'Forums',
-            link: '#',
-          },
-        ],
-      },
-      {
-        title: 'Project',
-        links: [
-          {
-            label: 'Contribute',
-            link: '#',
-          },
-          {
-            label: 'Media assets',
-            link: '#',
-          },
-          {
-            label: 'Changelog',
-            link: '#',
-          },
-          {
-            label: 'Releases',
-            link: '#',
-          },
-        ],
-      },
-      {
-        title: 'Community',
-        links: [
-          {
-            label: 'Join Discord',
-            link: '#',
-          },
-          {
-            label: 'Follow on Twitter',
-            link: '#',
-          },
-          {
-            label: 'Email newsletter',
-            link: '#',
-          },
-          {
-            label: 'GitHub discussions',
-            link: '#',
-          },
-        ],
-      },
-    ],
-  };
-
   return (
     <>
       <main>
